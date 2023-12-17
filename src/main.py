523c3f5386d76b053811fd50f56671422ec4d47c
Geeoon Chung <geeoon13@gmail.com>
2023-12-16 19:42:18 -0800

    Fixed improper package install

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
 import ffmpeg
+
+OUTPUT_WIDTH = 1080
+OUTPUT_HEIGHT = 1920
+ASPECT_RATIO = OUTPUT_WIDTH / OUTPUT_HEIGHT
+
+primary_vid = ffmpeg.input('test_files/primary.mp4')
+secondary_vid = ffmpeg.input('test_files/secondary.mp4')
