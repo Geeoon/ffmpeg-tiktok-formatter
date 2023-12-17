a77a5059d64094c71710a0868f072d80fe74c5c7
Geeoon Chung <geeoon13@gmail.com>
2023-12-17 03:59:59 -0800

    Added output to gitignore

--- src/main.py
+++ src/main.py
@@ -9,12 +9,24 @@ VIDEO_BITRATE = '512k'
 AUDIO_BITRATE= '192k'
 
 
+# vertical video
+# ff = FFmpeg(
+#     inputs={'test_files/primary.mp4': '-y', 'test_files/secondary.mp4': None},
+#     outputs={   'output.mp4': f'-shortest -filter_complex " \
+#                 [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={OUTPUT_WIDTH}:ih [primary]; \
+#                 [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={OUTPUT_WIDTH}:ih [secondary]; \
+#                 [primary][secondary] vstack=inputs=2 [outv];" \
+#                 -map [outv]:v -map 0:a -b:a {AUDIO_BITRATE} -b:v {VIDEO_BITRATE}'}
+# )
+
+# horizontal video
 ff = FFmpeg(
     inputs={'test_files/primary.mp4': '-y', 'test_files/secondary.mp4': None},
     outputs={   'output.mp4': f'-shortest -filter_complex " \
-                [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={OUTPUT_WIDTH}:ih [primary]; \
-                [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={OUTPUT_WIDTH}:ih [secondary]; \
-                [primary][secondary] vstack=inputs=2 [outv];" \
+                [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={math.trunc(OUTPUT_WIDTH / 2)}:{math.trunc(OUTPUT_HEIGHT / 2)} [primary]; \
+                [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={math.trunc(OUTPUT_WIDTH / 2)}:{math.trunc(OUTPUT_HEIGHT / 2)} [secondary]; \
+                [primary][secondary] hstack=inputs=2 [combined]; \
+                [combined]pad=w={OUTPUT_WIDTH}:h={OUTPUT_HEIGHT}:color=black [outv];" \
                 -map [outv]:v -map 0:a -b:a {AUDIO_BITRATE} -b:v {VIDEO_BITRATE}'}
 )
 
