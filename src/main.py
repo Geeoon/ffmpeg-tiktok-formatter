34a250a12925c3796f2dd6547a6266058a0bff85
Geeoon Chung <geeoon13@gmail.com>
2023-12-16 22:32:19 -0800

    Changed to ffmpy

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-import ffmpeg
 
 OUTPUT_WIDTH = 1080
 OUTPUT_HEIGHT = 1920
 ASPECT_RATIO = OUTPUT_WIDTH / OUTPUT_HEIGHT
+OUTPUT_FILE_NAME = 'output.mp4'
 
-primary_vid = ffmpeg.input('test_files/primary.mp4')
-secondary_vid = ffmpeg.input('test_files/secondary.mp4')
