6a4a8139fee6abc15a2b1e13f37d588441501706
Geeoon Chung <geeoon13@gmail.com>
2023-12-17 05:25:43 -0800

    Added additional height option

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 This project takes a widescreen or vertical clip and adds another clip so that it can be uploaded to TikTok, or any short-form content site, in a format that helps it retain attention longer.
 
 This repo is similar to the [TikTokClipReup](github.com/Geeoon/TikTokClipReup) repo, but uses FFmpeg directly through the ffmpy Python wrapper.
+
+## Dependencies:
+Use the following command to install all the dependencies:
+
+`sudo apt update && sudo apt install ffmpeg && pip install -U openai-whisper ffmpy`
+
+Alternatively, you can use pip to install the packages from the `requirements.txt` file.  In the base directory, run:
+
+`pip install -r requirements.txt`
--- src/main.py
+++ src/main.py
@@ -1,34 +1,41 @@
 import math
+import whisper
 from ffmpy import FFmpeg
 
 OUTPUT_WIDTH = 1080
 OUTPUT_HEIGHT = 1920
-ASPECT_RATIO = OUTPUT_WIDTH / OUTPUT_HEIGHT
+INPUT_FILE_DIRECTORY = "test_files"
 OUTPUT_FILE_NAME = 'output.mp4'
+PRIMARY_FILE_NAME= 'primary_speech.mp4'
+SECONDARY_FILE_NAME='secondary.mp4'
 VIDEO_BITRATE = '512k'
 AUDIO_BITRATE= '192k'
+ADDITIONAL_LENGTH = 300  # pixels of additional width/height for a primary video, can be negative
+
+
+# model = whisper.load_model("tiny.en")
 
 
 # vertical video
-# ff = FFmpeg(
-#     inputs={'test_files/primary.mp4': '-y', 'test_files/secondary.mp4': None},
-#     outputs={   'output.mp4': f'-shortest -filter_complex " \
-#                 [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={OUTPUT_WIDTH}:ih [primary]; \
-#                 [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={OUTPUT_WIDTH}:ih [secondary]; \
-#                 [primary][secondary] vstack=inputs=2 [outv];" \
-#                 -map [outv]:v -map 0:a -b:a {AUDIO_BITRATE} -b:v {VIDEO_BITRATE}'}
-# )
-
-# horizontal video
 ff = FFmpeg(
-    inputs={'test_files/primary.mp4': '-y', 'test_files/secondary.mp4': None},
-    outputs={   'output.mp4': f'-shortest -filter_complex " \
-                [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={math.trunc(OUTPUT_WIDTH / 2)}:{math.trunc(OUTPUT_HEIGHT / 2)} [primary]; \
-                [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={math.trunc(OUTPUT_WIDTH / 2)}:{math.trunc(OUTPUT_HEIGHT / 2)} [secondary]; \
-                [primary][secondary] hstack=inputs=2 [combined]; \
-                [combined]pad=w={OUTPUT_WIDTH}:h={OUTPUT_HEIGHT}:y=(oh-ih)/2:color=black [outv];" \
+    inputs={f'{INPUT_FILE_DIRECTORY}/{PRIMARY_FILE_NAME}': '-y -r 30', f'{INPUT_FILE_DIRECTORY}/{SECONDARY_FILE_NAME}': '-r 30'},
+    outputs={   f'{OUTPUT_FILE_NAME}': f'-shortest -filter_complex " \
+                [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2) + ADDITIONAL_LENGTH}, crop={OUTPUT_WIDTH}:ih [primary]; \
+                [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2) - ADDITIONAL_LENGTH}, crop={OUTPUT_WIDTH}:ih [secondary]; \
+                [primary][secondary] vstack=inputs=2 [outv];" \
                 -map [outv]:v -map 0:a -b:a {AUDIO_BITRATE} -b:v {VIDEO_BITRATE}'}
 )
+
+# horizontal video
+# ff = FFmpeg(
+#     inputs={f'{INPUT_FILE_DIRECTORY}/{PRIMARY_FILE_NAME}': '-y', f'{INPUT_FILE_DIRECTORY}/{SECONDARY_FILE_NAME}': None},
+#     outputs={   f'{OUTPUT_FILE_NAME}': f'-shortest -filter_complex " \
+#                 [0]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={math.trunc(OUTPUT_WIDTH / 2) + ADDITIONAL_LENGTH}:{math.trunc(OUTPUT_HEIGHT / 2)} [primary]; \
+#                 [1]scale=-2:{math.trunc(OUTPUT_HEIGHT / 2)}, crop={math.trunc(OUTPUT_WIDTH / 2) - ADDITIONAL_LENGTH}:{math.trunc(OUTPUT_HEIGHT / 2)} [secondary]; \
+#                 [primary][secondary] hstack=inputs=2 [combined]; \
+#                 [combined]pad=w={OUTPUT_WIDTH}:h={OUTPUT_HEIGHT}:y=(oh-ih)/2:color=black [outv];" \
+#                 -map [outv]:v -map 0:a -r 30 -b:a {AUDIO_BITRATE} -b:v {VIDEO_BITRATE}'}
+# )
 
 print(ff.cmd)
 
